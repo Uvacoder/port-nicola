7fd025879adc2a809a143a7fd18aedc458596c64
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-02-02 21:48:00 -0500

    Update FrontPage.js

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,4 +1,4 @@
-import {React, useState} from "react";
+import { React } from "react";
 import { Link } from "react-router-dom";
 
 import { createTheme, ThemeProvider } from "@mui/material/styles";
